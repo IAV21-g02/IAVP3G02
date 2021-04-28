73a870f0c46244d6eade9ad0e576ca041f085fdb
Amaro <ablest@ucm.es>
2021-04-28 11:53:02 +0200

    ADDDED: personajes y más texturas

---
+++ Decision/Assets/Scripts/Fantasma.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Fantasma_Estado : int
+{
+    Secuestrando = 1, Raptando, TocandoPiano,
+};
+
+public class Fantasma : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuscaCantante()
+    {
+
+    }
+}
