50cbd1f0d8e7362c105bade8179cfa17c57f92f8
Paula Martínez <pamart24@ucm.es>
2021-04-29 22:21:50 +0200

    ADDED: Animaciones del público hechas
    En el vestíbulo chocan entre ellos y por eso a veces se les ve corriendo sin sentido, pero si les forzamos a estar fijos no pasaría. Por otro lado, a veces los persj andan para atrás, pero no se si es por el mesh o porque rozan con el suelo...

--- Decision/Assets/PublicoAnimControl.cs
+++ Decision/Assets/PublicoAnimControl.cs
@@ -6,6 +6,7 @@ public class PublicoAnimControl : MonoBehaviour
 {
     Animator animator;
     Vector3 posIni;
+    Vector3 diff;
     public Transform objetivo;
 
     // Use this for initialization
@@ -13,25 +14,21 @@ public class PublicoAnimControl : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         posIni = transform.position;
+        diff = -transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //float h = Input.GetAxis("Horizontal");
-        //float v = Input.GetAxis("Vertical");
-        //bool fire = Input.GetButtonDown("Fire1");
-        //
-        //animator.SetFloat("Forward", v);
-        //animator.SetFloat("Strafe", h);
-        //animator.SetBool("Fire", fire);
-
         if (animator.GetBool("Sentada") && (transform.position.x > posIni.x + 0.1 || transform.position.x < posIni.x - 0.1)) {
+            transform.localPosition += diff;
+            //transform.position += diff;
             animator.SetBool("Correr", true);
             animator.SetBool("Sentada", false);
         }
         else if (animator.GetBool("Correr") && (transform.position.x < posIni.x + 0.1 && transform.position.x > posIni.x - 0.1))
         {
+            transform.localPosition -= diff;
             animator.SetBool("Sentada", true);
             animator.SetBool("Correr", false);
         }
@@ -40,7 +37,7 @@ public class PublicoAnimControl : MonoBehaviour
             animator.SetBool("Idle", true);
             animator.SetBool("Correr", false);
         }
-        else if (animator.GetBool("Idle") && (transform.position.x > objetivo.position.x + 0.5 || transform.position.x < objetivo.position.x - 0.5))
+        else if (animator.GetBool("Idle") && (transform.position.x > objetivo.position.x + 1.0 || transform.position.x < objetivo.position.x - 1.0))
         {
             animator.SetBool("Correr", true);
             animator.SetBool("Idle", false);
