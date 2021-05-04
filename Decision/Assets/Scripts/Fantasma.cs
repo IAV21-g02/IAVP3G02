74d9d57b2a574f1f45fbd82194c04650078f5c26
aurora <aurgar01@ucm.es>
2021-05-04 21:30:24 +0200

    intento de recursion
    Co-Authored-By: Amaro Blest Polo <36236348+Sounagix@users.noreply.github.com>

--- Decision/Assets/Scripts/Fantasma.cs
+++ Decision/Assets/Scripts/Fantasma.cs
@@ -12,18 +12,25 @@ public enum Estado : int
     BuscandoCantante, Secuestrando, TocandoMusica, RepararMuebles, Noqueado
 };
 
+[System.Serializable]
+public class salaVisitada
+{
+    public GameObject sala;
+    public bool visitado;
+}
+
 public class Fantasma : MonoBehaviour
 {
     public BehaviorTree behaviorTree;
     public int Estado = 0;
     public List<GameObject> path;
+    public salaVisitada[] salas;
 
     private NavMeshAgent navMeshAgent;
-
-    public GameObject[] barcas;
-    public bool[] estado;
     private SalaBehaviour salaActual;
     private GameObject salaObj;
+    private bool imposible;
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -95,12 +102,74 @@ public class Fantasma : MonoBehaviour
         }
     }
 
-    public void CalculaCamino(Transform pos)
+    public List<GameObject> CalculaCamino( bool imposible, SalaBehaviour salaAct , List<GameObject> path)
     {
-        if (salaActual.gameObject == salaObj) { 
-            path
-        }
+        //caso base
+        if (salaAct .gameObject == salaObj || imposible == true) return path;
+        marcarVisitado(salaAct.gameObject, true);
+            GameObject[] salasVecinas = salaAct.ConsultaVecinos();
+            int i = 0;
+            bool haycamino = false;
+            //comprovamos si podemos ir al destino
+            while (!haycamino && i < salasVecinas.Length)
+            {
+                if (salasVecinas[i] == salaObj)
+                {
+                    path.Add(salaAct.gameObject);
+                    haycamino = true;
+                    return path;
+                }
+                i++;
+            }
+            //si no hay camino directo al objetivo
 
+            for (i = 0; i < salasVecinas.Length; i++)
+            {
+                if (!isVisitado(salasVecinas[i]))
+                {
+                SalaBehaviour s = salasVecinas[i].GetComponent<SalaBehaviour>();
+                   path.Add(s.gameObject);
+                   path = CalculaCamino(imposible, s, path);
+                    
+                    //sacar la sala del path
+                    path.Remove(salaActual.gameObject);
+                    //devolvemos los valores originales
+                    salaActual = auxsala;
+                }
+
+            }
+        
+           
+        
+
+    }
+
+    private salaVisitada getSala(GameObject sala)
+    {
+        for(int i =0; i < salas.Length; i++)
+        {
+            if (salas[i].sala == sala) return salas[i];
+        }
+        return null;
+    }
+    private void marcarVisitado(GameObject sala, bool visit)
+    {
+        salaVisitada s = getSala(sala);
+        s.visitado = visit;
+        
+    }
+    private bool isVisitado(GameObject sala)
+    {
+        return getSala(sala).visitado;
+    }
+
+    private void reiniciaBusqueda()
+    {
+        for (int i = 0; i < salas.Length; i++)
+        {
+            salas[i].visitado = false;
+        }
+        
     }
 
 }
